0f819bf0d18d2e0ce988d5f93a1f0cf4a1c532a5
Erik Hinton <erikhinton@Eriks-MacBook-Pro-2.local>
2025-07-11 17:59:16 -0400

    hello world

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { LoadingScreen } from "./components/loading-screen";
+
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -43,6 +45,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return <Outlet />;
+}
+
+export function HydrateFallback() {
+  return <LoadingScreen />;
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Email poems" },
+    { name: "description", content: "I am a provocateur at heart." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <div>HELLO WORLD</div>;
 }
